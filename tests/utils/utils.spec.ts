35adc6b00b3cec7a7d6ec4754043dd94bd37932b
Thang Tran <thangtv611@gmail.com>
2021-06-19 10:41:42 +0700

    add test report && add circleci script

--- tests/utils/utils.spec.ts
+++ tests/utils/utils.spec.ts
@@ -1,10 +1,15 @@
 import { expect } from "chai";
-import {add} from '../../src/utils/utils';
+import {add, sub} from '../../src/utils/utils';
 
 describe('utils module', () => {
 
     it('should return add result correctly', () => {
         const result = add(1, 2);
         expect(result).to.equal(3);
-    })
+    });
+
+    it('should return sub failer', () => {
+        const result = sub(3, 1);
+        expect(result).to.equal(2);
+    });
 });
